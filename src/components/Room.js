a0006e9a211b7b8d9f8659511851023289465260
frankopkusianwar <okirorfrank3@gmail.com>
2020-10-30 18:04:30 +0300

    Modify room component

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { faTwitter } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import propTypes from 'prop-types';
 
-const Room = ({ book, removeBook }) => (
+const Room = () => (
   <div className="Room">
-    <p>room goes here</p>
+    <h6 className='title'>Deluxe Room With Balcony</h6>
+    <p className='description'>view: city view</p>
+    <div className='social-icons'>
+      social media links here
+    </div>
   </div>
 );
 
