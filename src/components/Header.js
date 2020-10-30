fd82005bf63aaab10dcec6d5202b36a8209f8754
frankopkusianwar <okirorfrank3@gmail.com>
2020-10-30 16:59:08 +0300

    Add sliding sidebar component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
   return (
     <div className="Header">
-      <FontAwesomeIcon icon={faBars} />
       <FontAwesomeIcon icon={faSearch} />
     </div>
   );
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,43 @@
 import React from 'react';
+import Sidebar from "react-sidebar";
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import App from './App';
 import Header from './Header'
+import SidebarContent from './SideBar'
 
-const Router = () => (
-  <BrowserRouter>
-    <Header />
-    <Switch>
-      <Route path="/" component={App} exact />
-    </Switch>
-  </BrowserRouter>
-);
+class Router extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidebarOpen: false
+    };
+    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+  }
+
+  onSetSidebarOpen(open) {
+    this.setState({ sidebarOpen: open });
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Header />
+        <Sidebar
+          sidebar={<SidebarContent />}
+          open={this.state.sidebarOpen}
+          onSetOpen={this.onSetSidebarOpen}
+          styles={{ sidebar: { background: "white", width: "15%"} }}
+        >
+      <FontAwesomeIcon icon={faBars} onClick={() => this.onSetSidebarOpen(true)}/>
+      </Sidebar>
+        <Switch>
+          <Route path="/" component={App} exact />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 export default Router;
