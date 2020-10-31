68f0e52cdd85dd8a053174349119883a9ad3aff5
frankopkusianwar <okirorfrank3@gmail.com>
2020-11-01 02:12:17 +0300

    Add style to room

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import room from '../assets/img/imagedet.PNG'
 import { faTwitter } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import propTypes from 'prop-types';
 
 const Room = () => (
   <div className="Room">
-    <h6 className='title'>Deluxe Room With Balcony</h6>
+    <div className="Room-circle"></div>
+    <div className="Room-img"><img src={room} alt="room" /></div>
+    <h5 className='title'>Deluxe Room With Balcony</h5>
     <p className='description'>view: city view</p>
     <div className='social-icons'>
       social media links here
