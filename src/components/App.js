3395ab3ab403a6442fcc39c5ad91640cb8281828
frankopkusianwar <okirorfrank3@gmail.com>
2020-11-01 00:58:09 +0300

    Add background color to home page

--- src/components/App.js
+++ src/components/App.js
@@ -4,21 +4,23 @@ import { Link } from 'react-router-dom';
 const App = () => {
   return (
     <div className="App">
-      <h3>MAKE A ROOM RESERVATION WITH US</h3>
-      <p>
-        We have 500 different rooms at our main branch. Today 200 rooms are available. 
-        We also have branches all over the country. If you want to find out and book from
-        our different branches. Please use the selector below.
-      </p>
-      <div>
-        <select>
-          <option>--select--</option>
-          <option>location 1</option>
-        </select>
-        <button>Book Now</button>
-        <p>Already have an account? <Link to="/login">Login</Link></p>
-        <h3>OR</h3>
-        <p>Sign up <Link to="/signup">Here</Link></p>
+      <div className="App-content">
+        <h3>MAKE A ROOM RESERVATION WITH US</h3>
+        <p>
+          We have 500 different rooms at our main branch. Today 200 rooms are available. 
+          We also have branches all over the country. If you want to find out and book from
+          our different branches. Please use the selector below.
+        </p>
+        <div className="form">
+          <select>
+            <option>--select--</option>
+            <option>location 1</option>
+          </select>
+          <button>Book Now</button>
+          <p>Already have an account? <Link to="/login">Login</Link></p>
+          <h4>OR</h4>
+          <p>Sign up <Link to="/signup">Here</Link></p>
+        </div>
       </div>
     </div>
   );
