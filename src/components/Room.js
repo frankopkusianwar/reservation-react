666ccc76ff552371f51484e3542c69c51dc23b13
frankopkusianwar <okirorfrank3@gmail.com>
2020-11-02 18:04:23 +0300

    Add room data to page

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import room from '../assets/img/imagedet.PNG'
+import image1 from '../assets/img/image1.jpg'
+import image2 from '../assets/img/image2.jpg'
+import image3 from '../assets/img/image3.jpg'
+import image4 from '../assets/img/image4.jpg'
+import image5 from '../assets/img/image5.jpg'
+
 import { faTwitter } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import propTypes from 'prop-types';
 
-const Room = () => (
+const Room = ({ result }) => (
   <div className="Room">
-    <div className="Room-circle"></div>
-    <div className="Room-img"><img src={room} alt="room" /></div>
-    <h5 className='title'>Deluxe Room With Balcony</h5>
-    <p className='description'>view: city view</p>
+    <div className="Room-img"><img src={image1} alt="room" /></div>
+    <h5 className='title'>{result.title}</h5>
+    <p className='description'>{result.bed}</p>
     <div className='social-icons'>
       social media links here
     </div>
