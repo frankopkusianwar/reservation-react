57a44fc145e349faa165878849cad940c6134e13
frankopkusianwar <okirorfrank3@gmail.com>
2020-11-02 16:00:02 +0300

    Add fetch room actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,12 @@ const TOGGLE_MENU = 'TOGGLE_MENU'
 const SHOW = 'SHOW'
 const CLOSE = 'CLOSE'
 const CLOSE_LOGIN_SUBMIT = 'CLOSE_LOGIN_SUBMIT'
+const FETCH_ROOMS_REQUEST = 'FETCH_ROOMS_REQUEST';
+const FETCH__SROOMS_UCCESS = 'FETCH_ROOMS_SUCCESS';
+const FETCH_ROOMS_FAILURE = 'FETCH_ROOMS_FAILURE';
+const FETCH_ONE_ROOM_REQUEST = 'FETCH_ONE_ROOM_REQUEST';
+const FETCH_ONE_ROOM__REQUEST_SUCCESS = 'FETCH_ONE_ROOM_REQUEST_SUCCESS';
+const FETCH_ONE_ROOM_REQUEST_FAILURE = 'FETCH_ONE_ROOM_REQUEST_FAILURE';
 
 export const toggleMenu = status => ({
   type: TOGGLE_MENU,
@@ -22,3 +28,55 @@ export const submitClose = (name) => ({
   type: CLOSE_LOGIN_SUBMIT,
   payload: name,
 });
+
+
+export const fetchRoomsRequest = () => ({
+  type: FETCH_ROOMS_REQUEST,
+});
+
+export const fetchRoomssSuccess = rooms => ({
+  type: FETCH_ROOMS_SUCCESS,
+  payload: rooms,
+});
+
+export const fetchRoomsFailure = error => ({
+  type: FETCH_ROOMS_FAILURE,
+  payload: error,
+});
+
+export const fetchOneRoomRequest = () => ({
+  type: FETCH_ONE_ROOM__REQUEST,
+});
+
+export const fetchOneRoomSuccess = room => ({
+  type: FETCH_ONE_ROOM_REQUEST_SUCCESS,
+  payload: room,
+});
+
+export const fetchOneRoomFailure = error => ({
+  type: FETCH_ONE_ROOM_REQUEST_FAILURE,
+  payload: error,
+});
+
+export const fetchRooms = () => dispatch => {
+  dispatch(fetchRoomsRequest);
+  axios.get(URL).then(response => {
+    const rooms = response.data;
+    dispatch(fetchRoomsSuccess(rooms));
+  }).catch(error => {
+    const errorMessage = error.message;
+    dispatch(fetchRoomsFailure(errorMessage));
+  });
+};
+
+export const fetchOneRoom = room_id => dispatch => {
+  const ROOMURL = ``;
+  dispatch(fetchOneRoom);
+  axios.get(ROOMURL).then(response => {
+    const room = response.data;
+    dispatch(fetchRoomSuccess(room));
+  }).catch(error => {
+    const errorMessage = error.message;
+    dispatch(fetchRoomFailure(errorMessage));
+  });
+};
