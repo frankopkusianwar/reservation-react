c473264ab083ea7eb4ac3bae166a8f606bf57466
frankopkusianwar <okirorfrank3@gmail.com>
2020-11-02 21:42:22 +0300

    Add signup reducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import sideBar from './sideBar'
 import modalReducer from './modalReducer'
 import roomReducer from './roomReducer'
+import signupReducer from './signupReducer'
 
 const rootReducer = combineReducers({
   sidebarOpen: sideBar,
   modalReducer,
   rooms: roomReducer,
+  signupReducer,
 });
 
 export default rootReducer;
