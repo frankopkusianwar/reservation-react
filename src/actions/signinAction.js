5f94eb93bc8e0edae20facbd1f5f5f3026340332
frankopkusianwar <okirorfrank3@gmail.com>
2020-11-03 12:06:01 +0300

    Add login action

---
+++ src/actions/signinAction.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-undef */
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+const LOGIN_USER = 'LOGIN_USER ';
+const LOGIN_USER_SUCESS = 'LOGIN_USER_SUCESS';
+const LOGIN_USER_FAILURE = 'LOGIN_USER_FAILURE';
+
+export const loginUser = () => ({
+  type: LOGIN_USER,
+  payload: true,
+});
+
+export const loginUserSucess = response => ({
+  type: LOGIN_USER_SUCESS,
+  payload: { msg: response.data },
+});
+
+export const loginUserfailure = error => ({
+  type: LOGIN_USER_FAILURE,
+  payload: error,
+});
+
+export const signinAction = credentials => (dispatch) => {
+  dispatch(loginUser());
+  return axios
+    .post('https://capstone-api-v1.herokuapp.com/auth/login', credentials)
+    .then((response) => {
+      sessionStorage.setItem('token', response.auth_token);
+      toast.dismiss();
+      dispatch(loginUserSucess(response));
+      toast.success('successfully loggedin!', {
+        autoClose: 12000,
+        hideProgressBar: false,
+      });
+    })
+    .catch(
+      (error) => {
+        dispatch({
+          type: LOGIN_USER_FAILURE,
+          payload: error,
+        });
+        toast.dismiss();
+        toast.error(`${error.response.data.message}`, {
+          autoClose: 3000,
+          hideProgressBar: true,
+        });
+      },
+    );
+};
+
+export default signinAction;
